f38f0fb21f5b8e972c56296b404ce637931b5eb2
Joshua Trono <josh.trono@gmail.com>
2023-05-23 15:30:33 -0400

    added the tests

--- Code/week 1/day2/src/test/java/StringUtilTest.java
+++ Code/week 1/day2/src/test/java/StringUtilTest.java
@@ -1,2 +1,22 @@
+package com.revature.tests;
+import org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import com.revature.tests.StringUtil.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class StringUtilTest {
+
+    @Test
+    void testReverse() {
+        String reversed = StringUtil.reverse("test");
+        assertEquals("tset", reversed);
+    }
+    @Test
+    void testFailedReverse() throws IllegalArgumentException {
+        assertThrows(IllegalArgumentException.class, () -> {String reversed = StringUtil.reverse(null);});
+    }
+
 }
